4502a2808228c88d93ff401fff1c1c7e834df07e
yinming0707 <111808372+yinming0707@users.noreply.github.com>
2022-09-29 17:33:24 +1300

    R file used to generate the demand scatter plot

---
+++ demand_visualisation.R
@@ -0,0 +1,13 @@
+library(tidyverse)
+# read in data and convert to tibble
+demand_data = read_delim("store_demand_plotting.txt",",")
+demand_tibble = as_tibble(demand_data)
+
+# plot scatter plot : x-axis weekdays, y-axis Saturdays
+g=ggplot(demand_tibble,aes(x=Weekdays,y=Saturday))
+g=g+geom_point(aes(color=Brand),size=3, alpha=0.7)
+g=g+labs(title="Weekday vs Saturday demand means",subtitle="Coloured by Branch",
+         y="number of pallets demanded on Saturdays", 
+         x="number of pallets demanded on weekdays")
+plot(g)
+
