4bf24e3b3b7ddbae91c9b04fde99f85099562576
yinming0707 <111808372+yinming0707@users.noreply.github.com>
2022-10-01 14:21:47 +1300

    Update demand_analysis.py
    added in docstrings and comments

--- demand_analysis.py
+++ demand_analysis.py
@@ -26,14 +26,24 @@ def get_demand_info():
         demand_dict = dict()
         for line in demand[1:]:
             demand_dict[line[0]] = [int(i) for i in line[1:]]
-        return date, demand_dict
+    return date, demand_dict
 
 
 def plot_total_demand(date, demand_dict):
+    """
+    Plot a bar chart of total demand of all supermarkets given on each day.
+
+    Parameters:
+    ---------
+    date: Python list
+        string representations of dates provided in the file
+    demand_dict: dictionary
+        keys correspond to name of supermarket, values are a list of number of pallets demanded on each day
+    """
     # sum elements at the same index for all lists in demand_dict
     demand_sum = [sum(x) for x in zip(*demand_dict.values())]
 
-    # plot a bar chart of total number of pallets demanded each day
+    # plot bar chart
     fig, ax = plt.subplots()
     fig.set_size_inches(20, 8, forward=True)
     ax.bar(x=date, height=demand_sum, width=0.4)
@@ -45,7 +55,17 @@ def plot_total_demand(date, demand_dict):
 
 
 def plot_demand_brand(date, demand_dict):
-    # create bar chart categorised by supermarket chain
+    """
+    Plot side-by-side bar chart on the same scale comparing demand of 3 supermarket brands on each day.
+
+    Parameters:
+    ---------
+    date: Python list
+        string representations of dates provided in the file
+    demand_dict: dictionary
+        keys correspond to name of supermarket, values are a list of number of pallets demanded on each day
+    """
+    # store each supermarket in new dictionaries dedicated to specific brand
     four_square, new_world, pak = dict(), dict(), dict()
     for key in demand_dict:
         if "Four Square" in key:
@@ -55,28 +75,43 @@ def plot_demand_brand(date, demand_dict):
         else:
             pak[key] = demand_dict[key]
 
+    # sum elements at the same index for all lists in each dictionary
     fs_sum = [sum(x) for x in zip(*four_square.values())]
     nw_sum = [sum(x) for x in zip(*new_world.values())]
     pks_sum = [sum(x) for x in zip(*pak.values())]
     df = pd.DataFrame({"Four Square": fs_sum, "New World": nw_sum, "Pak'n Save": pks_sum}, index=date)
 
-    df.plot.bar(figsize=(20,8))
+    # plot bar chart
+    df.plot.bar(figsize=(20, 8))
     plt.xlabel("Date")
     plt.ylabel("Number of pallets")
-    plt.title("Number of pallets demanded each day by 3 supermarket chains in 2022")
+    plt.title("Number of pallets demanded each day by 3 supermarket brands in 2022")
     plt.show()
 
 
 def write_demand_to_file(demand_dict):
+    """
+    Compute mean demand of each supermarket on weekdays and Saturdays and write to store_demand_plotting.txt
+    Round mean demand of each supermarket up and write to store_demand.txt
+
+    Parameters:
+    ---------
+    demand_dict: dictionary
+        keys correspond to name of supermarket, values are a list of number of pallets demanded on each day
+    """
+    # set up txt files and write in headings
     store_demandP = open("store_demand_plotting.txt", "w")
-    store_demand = open("store_demand_estimate.txt", "w")
+    store_demand = open("store_demand.txt", "w")
     store_demandP.write("Brand,Location,Weekdays,Saturday\n")
     store_demand.write("Brand,Location,Weekdays,Saturday\n")
     for key in demand_dict:
+        # filter out Sunday demands and compute mean
         weekday = [demand_dict[key][i] for i in range(len(demand_dict[key])) if i % 7 != 6 and i % 7 != 5]
         sat = [demand_dict[key][i] for i in range(len(demand_dict[key])) if i % 7 == 5]
         mean_wd = sum(weekday) / len(weekday)
         mean_sat = sum(sat) / len(sat)
+        
+        # separate the brand name and location
         if "New World" in key:
             brand = "New World"
             location = key.replace("New World ", "")
